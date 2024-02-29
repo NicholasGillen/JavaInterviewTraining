592cf62582f1dca40216cf5a5a379f54aef12687
NicholasGillen <ngillen@scottlogic.com>
2024-02-29 11:31:55 +0000

    amend Pair Summing solution

--- 4-pair-summing.java
+++ 4-pair-summing.java
@@ -12,10 +12,28 @@ import static org.junit.Assert.assertEquals;
 public class Solution {
 
   @Test
-  public void test1() {
+  public void addTo10() {
     int[] array = {5,4,5,6,7,2};
     int expectedPairsCount = 2;
-    int calculatedPairCount = calculatePairs(array);
+    int calculatedPairCount = calculatePairs(array, 10);
+
+    assertEquals(expectedPairsCount, calculatedPairCount);
+  }
+
+  @Test
+  public void addTo4() {
+    int[] array = {1,3,5,6,7,2,2,0,4};
+    int expectedPairsCount = 3;
+    int calculatedPairCount = calculatePairs(array, 4);
+
+    assertEquals(expectedPairsCount, calculatedPairCount);
+  }
+
+  @Test
+  public void addTo9() {
+    int[] array = {1,3,5,6,7,2,2,0,4,8,8,8};
+    int expectedPairsCount = 7;
+    int calculatedPairCount = calculatePairs(array, 9);
 
     assertEquals(expectedPairsCount, calculatedPairCount);
   }
@@ -24,7 +42,7 @@ public class Solution {
   public void noPairs() {
     int[] array = {5,4,1,3,75,12,8};
     int expectedPairsCount = 0;
-    int calculatedPairCount = calculatePairs(array);
+    int calculatedPairCount = calculatePairs(array, 10);
 
     assertEquals(expectedPairsCount, calculatedPairCount);
   }
@@ -33,7 +51,7 @@ public class Solution {
   public void allPairs() {
     int[] array = {5,5,5,5};
     int expectedPairsCount = 6;
-    int calculatedPairCount = calculatePairs(array);
+    int calculatedPairCount = calculatePairs(array, 10);
 
     assertEquals(expectedPairsCount, calculatedPairCount);
   }
@@ -42,7 +60,7 @@ public class Solution {
   public void heavyRightSidedArray() {
     int[] array = {8,8,2,2,8,8,8,8,8};
     int expectedPairsCount = 14;
-    int calculatedPairCount = calculatePairs(array);
+    int calculatedPairCount = calculatePairs(array, 10);
 
     assertEquals(expectedPairsCount, calculatedPairCount);
   }
@@ -51,22 +69,22 @@ public class Solution {
   public void heavyLeftSidedArray() {
     int[] array = {9,1,1,1,1,9,1,1,1};
     int expectedPairsCount = 14;
-    int calculatedPairCount = calculatePairs(array);
+    int calculatedPairCount = calculatePairs(array, 10);
 
     assertEquals(expectedPairsCount, calculatedPairCount);
   }
 
-  public int calculatePairs(int[] array) {
+  public int calculatePairs(int[] array, int givenNumber) {
     Arrays.sort(array);
     int count = 0;
     for (int i = 0; i < array.length-1; i++) {
       int currentNumber = array[i];
-      if (currentNumber >= 6) {
+      if (currentNumber >= (givenNumber/2) + 1) {
         break;
       }
       for (int j = i+1; j < array.length; j++) {
         int currentCheck = array[j];
-        if (currentNumber + currentCheck == 10) {
+        if (currentNumber + currentCheck == givenNumber) {
           count++;
         }
       }
@@ -78,3 +96,4 @@ public class Solution {
     JUnitCore.main("Solution");
   }
 }
+
